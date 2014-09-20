13ac9fc159f3e5f2e95598564d424553bf8b93a0
ikeyasu <ikeyasu@gmail.com>
2014-09-20 15:14:56 +0900

    Imp. add unittests
    This unittests uses GMock.
    If you need very simple sample of Arduino unittest using GMock,
    please refer to https://github.com/ikeyasu/arduino-gmock-sample

---
+++ test/milkcarton-1_unittest.cc
@@ -0,0 +1,43 @@
+/* Copyright 2014 http://switchdevice.com */
+/* vim: set ts=2  sw=2 sts=2 et si: */
+/* astyle --style=google --indent=spaces=2 --pad-oper milkcarton-1_unittest.cc*/
+
+#define DEBUG false
+
+#include "gtest/gtest.h"
+#include "arduino-mock/Arduino.h"
+#include "arduino-mock/Serial.h"
+Serial_ Serial;
+
+#include "arduino-mock/Spark.h"
+Spark_ Spark;
+
+#include "../milkcarton-1.ino"
+
+using ::testing::StrCaseEq;
+
+TEST(setup, normal) {
+  ArduinoMock* arduinoMock = arduinoMockInstance();
+  SerialMock* serialMock = serialMockInstance();
+  EXPECT_CALL(*serialMock, begin(9600));
+  setup();
+  delete serialMock;
+  delete arduinoMock;
+}
+
+TEST(detectMoving, normal) {
+  EXPECT_FALSE(detectMoving(0, 0, 0));
+  EXPECT_TRUE(detectMoving(100, 100, 100));
+  EXPECT_TRUE(detectMoving(0, 0, 0));
+  EXPECT_FALSE(detectMoving(10, 10, 10));
+  EXPECT_FALSE(detectMoving(0, 0, 0));
+  EXPECT_TRUE(detectMoving(100, 0, 0));
+}
+
+TEST(publish, normal) {
+  SparkMock* sparkMock = sparkMockInstance();
+  EXPECT_CALL(*sparkMock,
+              publish("accelerationDisptched", StrCaseEq("1,1,1,1")));
+  publish(1, 1, 1);
+  delete sparkMock;
+}

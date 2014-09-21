8aec4b1fefea340ffa4e6b4be511178b83f8dddf
ikeyasu <ikeyasu@gmail.com>
2014-09-21 13:20:28 +0900

    Fix. fix unittest for loop

--- test/milkcarton-1_unittest.cc
+++ test/milkcarton-1_unittest.cc
@@ -15,17 +15,20 @@ Spark_ Spark;
 #include "../milkcarton-1.ino"
 
 using ::testing::StrCaseEq;
+using ::testing::Return;
 
 TEST(setup, normal) {
-  ArduinoMock* arduinoMock = arduinoMockInstance();
   SerialMock* serialMock = serialMockInstance();
   EXPECT_CALL(*serialMock, begin(9600));
   setup();
-  delete serialMock;
-  delete arduinoMock;
+  releaseSerialMock();
 }
 
 TEST(detectMoving, normal) {
+  // reset old value
+  detectMoving(0, 0, 0);
+  detectMoving(0, 0, 0);
+
   EXPECT_FALSE(detectMoving(0, 0, 0));
   EXPECT_TRUE(detectMoving(100, 100, 100));
   EXPECT_TRUE(detectMoving(0, 0, 0));
@@ -39,5 +42,38 @@ TEST(publish, normal) {
   EXPECT_CALL(*sparkMock,
               publish("accelerationDisptched", StrCaseEq("1,1,1,1")));
   publish(1, 1, 1);
-  delete sparkMock;
+  releaseSparkMock();
+}
+
+TEST(loop, normal) {
+  // reset old value
+  detectMoving(0, 0, 0);
+  detectMoving(0, 0, 0);
+
+  ArduinoMock* arduinoMock = arduinoMockInstance();
+  SparkMock* sparkMock = sparkMockInstance();
+  EXPECT_CALL(*arduinoMock, analogRead(5))
+    .WillOnce(Return(0));
+  EXPECT_CALL(*arduinoMock, analogRead(4))
+    .WillOnce(Return(0));
+  EXPECT_CALL(*arduinoMock, analogRead(3))
+    .WillOnce(Return(0));
+  EXPECT_CALL(*arduinoMock, delay(1000));
+  loop();
+
+  EXPECT_CALL(*arduinoMock, analogRead(5))
+    .WillOnce(Return(110));
+  EXPECT_CALL(*arduinoMock, analogRead(4))
+    .WillOnce(Return(110));
+  EXPECT_CALL(*arduinoMock, analogRead(3))
+    .WillOnce(Return(110));
+  EXPECT_CALL(*arduinoMock, millis())
+    .WillOnce(Return(0));
+  EXPECT_CALL(*sparkMock,
+              publish("accelerationDisptched", StrCaseEq("1,6e,6e,6e")));
+  EXPECT_CALL(*arduinoMock, delay(30));
+  loop();
+
+  releaseSparkMock();
+  releaseArduinoMock();
 }
